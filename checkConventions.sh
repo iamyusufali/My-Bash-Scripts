e6178f526bd32f76dc3d19d324ed2608528871cb
Yusuf Ali <yali98319@gmail.com>
2020-03-18 12:47:33 +0530

    Added new script

---
+++ checkConventions.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+ 
+
+# Prompt for directory name.
+read -p "Enter directory name: " MAINDIR
+
+
+# To correct unwanted space 
+function correctUnwantedSpace () {
+  sed -i 's|  (| (|g' $FILE
+  sed -i 's|)  |) |g' $FILE
+  sed -i 's|  }| }|g' $FILE
+  sed -i 's|{  |{ |g' $FILE
+  sed -i 's|=  {|= {|g' $FILE
+  sed -i 's|,  |, |g' $FILE
+  sed -i 's|:  |: |g' $FILE
+  sed -i 's| ;|;|g' $FILE
+  sed -i 's| \.|.|g' $FILE
+}
+
+
+# To add Indentation
+function addIndentation () {
+  sed -i 's|(| (|g' $FILE
+  sed -i 's|)|) |g' $FILE
+  sed -i 's|}| }|g' $FILE
+  sed -i 's|{|{ |g' $FILE
+  sed -i 's|={|= {|g' $FILE
+  sed -i 's|,|, |g' $FILE
+  sed -i 's|:|: |g' $FILE
+  correctUnwantedSpace
+}
+
+
+# Check if the directory entered exists. 
+# Then find & store all the files with '.js' extension.
+# Then invoke the function declared above to add indentation. 
+
+if [ -e "${MAINDIR}" ] 
+then
+  JSFILES=$(find ./$MAINDIR -type f -name '*.js')
+  for FILE in ${JSFILES}
+    do
+    addIndentation
+  done
+else 
+  echo "Directory does not exists in current location!"
+fi
